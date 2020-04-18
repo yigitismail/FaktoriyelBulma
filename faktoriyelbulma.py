6c467fd5a9ca830f781b69b23f4f79a80c505b38
İsmail Yiğit <smlygt@gmail.com>
2020-04-18 17:39:00 +0300

    son

--- faktoriyelbulma.py
+++ faktoriyelbulma.py
@@ -5,6 +5,23 @@ Faktöriyel Bulma Programı
 Çıkmak için q'ya basınız...
 ****************************
 """)
-ffff
-jjjfjfjjjjjf
-jsjlksf sfljksdf sdnflkjsdfsd
+
+while True:
+    sayi = input("Bir sayı girin: ")
+    if(sayi =="q"):
+        print("Programdan çıkılıyor...")
+        break
+    else:
+        sayi = int(sayi)
+        faktoriyel = 1
+
+        for i in range(2,sayi+1):
+            faktoriyel *=i
+
+        print(faktoriyel)
+
+
+
+
+
+
